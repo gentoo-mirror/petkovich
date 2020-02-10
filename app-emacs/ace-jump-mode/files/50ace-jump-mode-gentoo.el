afd2efc7b48fdb418ac7572c891ba5ac264fbbe7
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2020-02-10 17:54:39 -0500

    Drop old ebuilds.

--- app-emacs/ace-jump-mode/files/50ace-jump-mode-gentoo.el
+++
@@ -1,3 +0,0 @@
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'ace-jump-mode "ace-jump-mode" "Emacs quick move minor mode" t)
-(autoload 'ace-jump-mode-pop-mark "ace-jump-mode" "Emacs quick move minor mode" t)
